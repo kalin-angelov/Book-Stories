502d267b41db2d490ff7f43416ed865d92fb0e6e
Kalizar <kalinangelov@abv.bg>
2024-04-03 18:50:57 +0300

    Create hone page

---
+++ store/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Discounts from '../Discounts/Discounts';
+import Gallery from '../Gallery/Gallery';
+import GoUpBtn from '../../shared/GoUpBtn/GoUpBtn';
+
+const Home = () => {
+  return (
+    <main>
+      <Discounts />
+      <Gallery />
+      <GoUpBtn />
+    </main>
+  );
+};
+
+export default Home;
