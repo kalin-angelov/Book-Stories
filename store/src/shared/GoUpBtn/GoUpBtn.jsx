4bb5b87bc220268194c29559807ac310aa4def18
Kalizar <kalinangelov@abv.bg>
2024-04-03 18:48:38 +0300

    Create Go to top btn

---
+++ store/src/shared/GoUpBtn/GoUpBtn.jsx
@@ -0,0 +1,42 @@
+import styles from './style/GoUpBtn.module.css';
+
+import { useEffect, useState } from 'react';
+
+const GoUpBtn = () => {
+    const [showBtn, setShowBtn] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            (window.scrollY > 500) ?
+                setShowBtn(true)
+            :
+                setShowBtn(false);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    },[]);
+
+    const moveToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+    
+    return (
+        <button
+            style={{display: showBtn ? 'block' : 'none'}}
+            className={styles.goUpBtn}
+            onClick={moveToTop}
+        >
+            <i className='fa-solid fa-circle-chevron-up'></i>
+        </button>
+    );
+};
+
+export default GoUpBtn;
