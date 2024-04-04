a85b54f952afd6e91ebf06b7803b87dbceff412d
Kalizar <kalinangelov@abv.bg>
2024-04-04 19:20:41 +0300

    Update App.jsx

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Product from './components/Product/Product';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Product />}/>
       </Routes>
       <Footer />
     </>
