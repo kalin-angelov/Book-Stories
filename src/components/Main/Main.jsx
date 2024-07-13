1c56a021db7c86bcee409b6258ef571db4a126b4
Kalizar <kalinangelov@abv.bg>
2024-07-13 12:17:24 +0300

    add

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import Gallery from '../Gallery/Gallery';
+import GoUpBtn from '../../Shared/GoUpBtn/GoUpBtn';
+
+const Main = () => {
+    return (
+        <main>
+
+            <Gallery />
+            <GoUpBtn />
+            
+        </main>
+    );
+};
+
+export default Main;
