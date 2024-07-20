23a088831c94eb3515c087c89294dde8a5990ea6
Kalizar <kalinangelov@abv.bg>
2024-07-20 21:26:43 +0300

    save

--- src/utils/discountCalculation.tsx
+++
@@ -1,4 +0,0 @@
-export const discountCalculation = (regularPrice: number, discountPercent: number): string =>  {
-    let calculation: number = (regularPrice - (regularPrice * (discountPercent / 100)));
-    return calculation.toFixed(2);
-};
