70419eb67276a40844df574b4feb499cb7d45050
Kalizar <kalinangelov@abv.bg>
2024-07-21 13:05:28 +0300

    Create Footer.tsx

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,57 @@
+import styles from "./Footer.module.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footerMainContainer}>
+
+            <section className={styles.socialMediaLinks}>
+
+                <h3>Book Stories</h3>
+
+                <Link to="https://www.facebook.com/" target="_blank">
+                    <i className="fa-brands fa-facebook"></i>
+                </Link>
+
+                <Link to="https://www.instagram.com/" target="_blank">
+                    <i className="fa-brands fa-instagram"></i>
+                </Link>
+
+                <Link to="https://www.youtube.com/" target="_blank">
+                    <i className="fa-brands fa-youtube"></i>
+                </Link>
+                
+                <Link to="https://www.tiktok.com/" target="_blank">
+                    <i className="fa-brands fa-tiktok"></i>
+                </Link>
+
+            </section>
+
+            <div className={styles.siteInformation}>
+
+                <article>
+                    <h4>Contacts</h4>
+                    <ul>
+                        <li>Location: <span></span></li>
+                        <li>Phone: <span>001 122 3344</span></li>
+                        <li>Mail: <span>bookstories@gmail.com</span></li>
+                    </ul>
+                </article>
+
+                <article>
+                    <h4>About Us</h4>
+                    <ul>
+                        <li>Work Time</li>
+                        <li>About</li>
+                        <li>Team</li>
+                    </ul>
+                </article>
+
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
