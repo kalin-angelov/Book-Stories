945af9a1999d9ccaf0727f681dbc3b9acee42735
Kalizar <kalinangelov@abv.bg>
2024-07-21 13:05:53 +0300

    Create Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import styles from "./Header.module.css";
+
+import { Link } from "react-router-dom";
+
+import Navigation from "./Navigation/Navigation";
+import Cart from "./Cart/Cart";
+import ProfileMenu from "./ProfileMenu/ProfileMenu";
+
+const Header = () => {
+
+    return (
+        <header>
+            <div className={styles.headerSectionOne}>
+
+                <Navigation />
+
+                <Link className={styles.title} to="/" >Book Stories</Link>
+
+            </div>
+            
+            <div className={styles.headerSectionTwo}>
+
+                <ProfileMenu />
+
+                <Cart />
+                
+            </div>
+        </header>
+    );
+};
+
+export default Header;
