39e0773501c238041262b2d99206c156a96bbacd
Kalizar <kalinangelov@abv.bg>
2024-07-23 11:39:43 +0300

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import styles from "./Home.module.css";
 
 import Gallery from "../Gallery/Gallery";
+import AuthorsList from "../Authors/AuthorsList/AuthorsList";
 
 const Home = () => {
     return (
         <main className={styles.main}>
             <Gallery />
+            <AuthorsList />
         </main>
     );
 };
