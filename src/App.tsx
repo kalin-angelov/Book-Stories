645ac678e0da5092642136d669d02a937f3ccbf6
Kalizar <kalinangelov@abv.bg>
2024-07-26 14:21:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import EntryPage from "./components/EntryPage/EntryPage";
 import AuthorBio from "./components/Authors/AuthorBio/AuthorBio";
+import Product from "./components/Product/Product";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/entry" element={<EntryPage />} />
                     <Route path="/author_bio" element={<AuthorBio />} />
+                    <Route path="/product" element={<Product />} />
                 </Routes>
                 <Footer />
             </>
