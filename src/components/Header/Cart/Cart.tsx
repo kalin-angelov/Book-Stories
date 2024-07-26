fa886be8c2abc809ca468c90e0c16bde714902a6
Kalizar <kalinangelov@abv.bg>
2024-07-26 14:21:11 +0300

    Update Cart.tsx

--- src/components/Header/Cart/Cart.tsx
+++ src/components/Header/Cart/Cart.tsx
@@ -32,14 +32,14 @@ const Cart = ( ) => {
 
   return (
     <>
-        <button className={styles.cartBtn} onClick={onCartClick}>
+        <button className={styles.cart_btn} onClick={onCartClick}>
             <img src="/images/icon-cart.svg" alt="shopping cart icon" />
-            {productsInCart != 0 && <span className={styles.numOfProducts}>{productsInCart}</span>}
+            {productsInCart != 0 && <span className={styles.num_of_products}>{productsInCart}</span>}
         </button>
 
-        <section className={cartOpen ? styles.cartContainer : styles.cartContainerClose} ref={cartRef}>
+        <section className={cartOpen ? styles.cart_container : styles.cart_container_close} ref={cartRef}>
             <h3>Cart</h3>
-            <div className={styles.cartContent}>
+            <div className={styles.cart_content}>
                
                 <p className={styles.message}>Your cart is empty.</p>
                 
