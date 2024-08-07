491796d330e49b97136c4d3421e7f889239835cb
Kalizar <kalinangelov@abv.bg>
2024-08-07 13:28:12 +0300

    Delete Context.tsx

--- src/context/Context.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const Context = createContext({
-    cartOpen: false,
-    setCartOpen: (_value: boolean) => {},
-    windowSize: window.innerWidth,
-    setWindowSize: (_value: number) => {}
-});
