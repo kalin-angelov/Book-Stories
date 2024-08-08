0ccd7a3635cdb07296c8cb630f33c9da51699917
Kalizar <kalinangelov@abv.bg>
2024-08-08 14:03:09 +0300

    Update EntryPage.tsx

--- src/components/EntryPage/EntryPage.tsx
+++ src/components/EntryPage/EntryPage.tsx
@@ -1,11 +1,13 @@
 import styles from "./EntryPage.module.css";
 
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import Register from "./Register/Register";
+import Login from "./Login/Login";
 
 const EntryPage = () => {
     const location = useLocation();
-    const navigate = useNavigate();
     const form = location.state;
     const [formState, setFormState] = useState<boolean>(form === "sing_in" ? true : false);
     
@@ -14,50 +16,22 @@ const EntryPage = () => {
         formType === "login" ? setFormState(true) : setFormState(false);
     };
 
-    const onSubmit = () => {
-        navigate("/");
-    };
-
     return (
         <main className={styles.main}>
 
-            <form className={styles.entry_form}>
+            <ul className={styles.form_titles}>
+                <li id="login" style={formState === true ? {borderBottom: "1px solid #52feaa"} : {borderBottom: "unset"}} onClick={(e) => changeForm(e)}>Login</li>
+                <li id="register"style={formState === false ? {borderBottom: "1px solid #52feaa"} : {borderBottom: "unset"}} onClick={(e) => changeForm(e)}>Register</li>
+            </ul>
 
-                <ul className={styles.form_titles}>
-                    <li id="login" style={formState === true ? {borderBottom: "1px solid #52feaa"} : {borderBottom: "unset"}} onClick={(e) => changeForm(e)}>Login</li>
-                    <li id="register"style={formState === false ? {borderBottom: "1px solid #52feaa"} : {borderBottom: "unset"}} onClick={(e) => changeForm(e)}>Register</li>
-                </ul>
+            {
+                formState ? 
 
-                {
-                    formState ? 
+                <Login />
+            :
+                <Register />
+            }
 
-                    <fieldset className={styles.login_form}>
-
-                        <input type="text" name="email" id="email" placeholder="Email"/>
-
-                        <input type="text" name="password" id="password" placeholder="Password"/>
-
-                    </fieldset>
-                :
-                    <fieldset className={styles.register_form}>
-   
-                        <input type="text" name="firstName" id="firstName" placeholder="Name"/>
-        
-                        <input type="text" name="lastName" id="lastName" placeholder="Last name"/>
-   
-                        <input type="text" name="email" id="email" placeholder="Email"/>
-
-                        <input type="text" name="password" id="password" placeholder="Password"/>
-   
-                        <input type="text" name="rePassword" id="rePassword" placeholder="Confirm password"/>
-    
-                    </fieldset>
-                    
-                }
-
-                <button className={styles.submit_btn} onClick={onSubmit} type="submit">Submit</button>
-
-            </form>
         </main>
     );
 };
