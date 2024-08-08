62a3ee454f07a6f36ef4a9c0e88cfca14ac5b4f6
Kalizar <kalinangelov@abv.bg>
2024-08-08 14:03:20 +0300

    Create userType.tsx

---
+++ src/types/userType.tsx
@@ -0,0 +1,12 @@
+export type UserReg = {
+    firstName: string,
+    lastName: string,
+    mail: string,
+    password: string,
+    rePass: string,
+};
+
+export type UserLog = {
+    mail: string,
+    password: string,
+};
