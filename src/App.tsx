af0a2b7ab6dcae446a37b8a3b799a69a8087eff6
Kalizar <kalinangelov@abv.bg>
2024-08-10 12:20:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import { GlobalContext, GlobalContextType } from "./context/Context";
+
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import EntryPage from "./components/EntryPage/EntryPage";
@@ -12,17 +14,27 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
     const [windowSize, setWindowSize] = useState<number>(window.innerWidth);
+    const [cartOpen, setCartOpen] = useState<boolean>(false);
+
+    const initValue: GlobalContextType  = {
+        windowSize,
+        setWindowSize,
+        cartOpen,
+        setCartOpen
+    };
 
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/entry" element={<EntryPage />} />
-                <Route path="/author_bio" element={<AuthorBio />} />
-                <Route path="/product" element={<Product />} />
-            </Routes>
-            <Footer />
+            <GlobalContext.Provider value={initValue}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/entry" element={<EntryPage />} />
+                    <Route path="/author_bio" element={<AuthorBio />} />
+                    <Route path="/product" element={<Product />} />
+                </Routes>
+                <Footer />
+            </GlobalContext.Provider>
         </>
     );
 };
